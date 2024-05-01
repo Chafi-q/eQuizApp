eddbfea19455119ae63edf67a6bea82a10d90640
Chafiq <chafik.m149@gmail.com>
2024-05-02 00:09:49 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { EnseignantComponent } from './enseignant/enseignant.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:LoginComponent},
+  {path:"login",component:LoginComponent},
+  {path:"home",component:AdminTemplateComponent},
+  {path:"admin",component:AdminTemplateComponent,children:[
+    {path:"etudiants",component:EtudiantComponent},
+    {path:"enseignants",component:EnseignantComponent},
+    {path:"dashboard",component:DashboardComponent},
+
+]},
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatDrawerContainer, MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { MatDivider } from '@angular/material/divider';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { EnseignantComponent } from './enseignant/enseignant.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminTemplateComponent,
+    EtudiantComponent,
+    EnseignantComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,CommonModule,MatIconModule,MatListModule,MatButtonModule,MatMenuModule,
+    MatDrawerContainer,MatDivider,MatToolbarModule,CommonModule,MatSidenavModule,MatTableModule,
+    MatCardModule,MatFormFieldModule,MatPaginatorModule,HttpClientModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
